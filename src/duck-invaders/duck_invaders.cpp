bb0b0015890dac3bd075bf556a6c5069a4a6956e
seif-attia <seifislam871@gmail.com>
2025-11-26 03:46:47 +0200

    duck

--- src/duck-invaders/duck_invaders.cpp
+++ src/duck-invaders/duck_invaders.cpp
@@ -4,34 +4,74 @@
 #include "duck.h"
 
 
-//Functions
+/**********************************************************************/
+		// CUSTOM FUNCTION REGION OR CLASSES OR STRUCTS
+/**********************************************************************/
 
 
 
 
 
+/**********************************************************************/
 
 
-
-
-void playDuckInvaders()
+void playDuckInvaders() // main
 {
-
+	/**********************************************************************/
+				// DECLARE ALL THE VARIABLES HERE LIKE MAIN
+	/**********************************************************************/
 	const int screenWidth = 1200;
 	const int screenHeight = 800;
-
 
 	SetWindowSize(screenWidth, screenHeight);
 
 	bool playing = true;
 	Button quitButton("assets/sprites/white_button.png", { 50, 50 / 2 }, 0.2);
 
+
+
+
+
+
+	/**********************************************************************/
+
+
+	// Game Loop
 	while (playing)
 	{
+
+		/**********************************************************************/
+				// CHECKING FOR VARIABLES THAT NEED TO BE CONSTANTLY UPDATED
+		/**********************************************************************/
+		// Initilization of variables
 		Vector2 mousePosition = GetMousePosition();
 		bool mousePressed = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
 
 
+
+		/**********************************************************************/
+							// INPUT REGION
+		/**********************************************************************/
+
+		// Check for user input
+		if (quitButton.isPressed(mousePosition, mousePressed))
+		{
+			playing = false;
+		}
+
+		// Returns to the main menu if the X button is pressed
+		if (WindowShouldClose() == true)
+		{
+			playing = false;
+		}
+
+
+
+		/**********************************************************************/
+							// DRAWING REGION
+		/**********************************************************************/
+
+		// Drawing
 		BeginDrawing();
 
 		ClearBackground(BLACK);
@@ -41,12 +81,11 @@ void playDuckInvaders()
 		quitButton.Draw();
 		DrawText("quit", quitButton.getPostion().x + 12, quitButton.getPostion().y + 6, 20, BLACK);
 
-		if (quitButton.isPressed(mousePosition, mousePressed))
-		{
-			playing = false;
-		}
+
 
 
 		EndDrawing();
+
+		/**********************************************************************/
 	}
 }
